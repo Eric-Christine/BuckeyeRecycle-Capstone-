185c515a50b2db2678a47707d38b8b10b301982f
Eric Chrsitine <ericjchristine@gmail.com>
2021-01-04 21:03:33 -0700

    first-commit

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// Drawer
+
+import HomeScreen from './src/screens/screens/HomeScreen';
+import ComponentsScreen from './src/screens/screens/ComponentsScreen';
+import ListScreen from './src/screens/screens/ListScreen';
+
+import ShopScreen from './src/screens/screens/ShopScreen'
+import CounterScreen from './src/screens/screens/CounterScreen'
+import ColorScreen from './src/screens/screens/ColorScreen'
+import SquareScreen from './src/screens/screens/SquareScreen'
+import LoggedItems from './src/screens/screens/LoggedItems'
+import LogItemScreen from './src/screens/screens/LogItemScreen'
+import RewardScreen from './src/screens/screens/RewardScreen'
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Shop: ShopScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
+    Log: LoggedItems,
+    newLog: LogItemScreen,
+    Reward: RewardScreen
   },
-});
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'Home',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
